e2d3a7368bd0388056911da3881a974d4063071f
Basil Hess <bhe@zurich.ibm.com>
2025-01-23 09:59:38 +0100

    updates for liboqs

--- mlkem/sampling.c
+++ mlkem/sampling.c
@@ -7,8 +7,8 @@
 
 #include "arith_backend.h"
 #include "debug.h"
-#include "fips202/fips202.h"
-#include "fips202/fips202x4.h"
+#include "fips202.h"
+#include "fips202x4.h"
 #include "sampling.h"
 #include "symmetric.h"
 
